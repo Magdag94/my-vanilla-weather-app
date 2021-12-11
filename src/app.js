6977387da5ca4301e60741caafb1f61b6b4f918f
Magdag94 <magdag.niemiec@gmail.com>
2021-12-11 16:37:19 -0600

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-alert("Hello World");
+function displayTemperature(response) {
+    console.log(response.data);
+
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "425d0bc2c7e02d9360a84d164c7a3cef";
+let apiUrl =
+  `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+  console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
