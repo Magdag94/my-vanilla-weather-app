ebc48d01a3b48ec3ffe275e3b65c6b20e39a3e8e
Magdag94 <magdag.niemiec@gmail.com>
2021-12-20 22:02:13 -0600

    Added the weather icon to weather forecast

--- src/app.js
+++ src/app.js
@@ -37,7 +37,17 @@ function displayForecast (response) {
     <div class="column">
     <div class="temperature-forecast-date">${formatDay(forecastDay.dt)}</div>
     </div>
-    ${index}
+    <div class="column">
+    <div class="temperature-forecast-icon">
+    <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="12"
+        />
+        </div>
+        </div>
     <div class="column">
     <div class="temperatures-forecast">
     <span class="temperature-forecast-max">
@@ -110,14 +120,17 @@ search("New York");
 
 function displayFahrenheitTemperature(event) {
     event.preventDefault();
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
     let fahrenheitTemperature = (14 * 9) / 5 + 32;
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-    alert("Link clicked");
 }
 
 function displayCelsiusTemperature(event) {
     event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
